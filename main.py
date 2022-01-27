bf3f11ef63ad1313785acf7e45634cd50f68f580
MrStass <stasikpyskir2@gmail.com>
2022-01-27 11:58:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+double_integer = int(input('Введите число: '))
+print('Вы ввели:', double_integer)
+print('Результат:', double_integer * 2)
+###############################
+
+double_float = float(input('Введите число с запятой: '))
+print('Вы ввели:', double_float)
+print('Результат:', double_float * 2)
+###############################
+
+double_str = str(input('Введите строку: '))
+print('Вы ввели:', double_str)
+print('Результат:', double_str * 2)
+
+
